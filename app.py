5a166cb6cd9c9b1c283ba569b8ce55e68a510303
Chris Parfitt <parfitt.chris7@gmail.com>
2020-01-20 16:04:48 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from webscraper import get_cnn, get_fox, get_NYT, get_huff, get_nbc
+from flask_cors import CORS
+import os
+
+# Init App
+
+app = Flask(__name__, static_folder="./frontend",
+            template_folder="./static/src")
+
+CORS(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init Marshmallow
+ma = Marshmallow(app)
+
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg': 'Hello World'})
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    site = db.Column(db.String(100))
+    title = db.Column(db.String(500))
+    url = db.Column(db.String(500))
+    number = db.Column(db.Integer)
+
+    def __init__(self, site, title, url, number):
+        self.site = site
+        self.title = title
+        self.url = url
+        self.number = number
+
+# Article Schema
+
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'site', 'title', 'url', 'number')
+
+
+# Init Schema
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+# Create an Article
+
+
+def add_article(obj, organization):
+    for article in obj:
+        site = article['site']
+        title = article['title']
+        url = article['url']
+        number = article['number']
+
+        current = Article.query.filter(
+            Article.number == number, Article.site == organization).first()
+
+        if current:
+            current.site = site
+            current.title = title
+            current.url = url
+            current.number = number
+            db.session.commit()
+        else:
+            new_article = Article(site, title, url, number)
+            db.session.add(new_article)
+            db.session.commit()
+
+
+@app.route('/article/', methods=['GET'])
+def get_articles():
+    all_articles = Article.query.all()
+    result = articles_schema.dump(all_articles)
+    return jsonify(result)
+
+
+@app.route('/article/<id>', methods=['GET'])
+def get_article(id):
+    article = Article.query.get(id)
+    result = article_schema.dump(article)
+    return jsonify(result)
+
+
+# cnn_results = get_cnn()
+# fox_results = get_fox()
+# fox_results = get_NYT()
+# huff_results = get_huff()
+# nbc_results = get_nbc()
+
+
+# add_article(cnn_results, 'CNN')
+# add_article(fox_results, 'FOX')
+# add_article(fox_results, 'NYTimes')
+# add_article(huff_results, 'Huffington Post')
+# add_article(nbc_results, 'NBC News')
+
+
+# Run Server
+if __name__ == '__main__':
+    app.run(threaded=True, port)
+    # app.run(debug=True);
+
---
+++ webscraper.py
@@ -0,0 +1,191 @@
+from selenium import webdriver
+
+
+# CNN--------------------------------
+def get_cnn():
+    driver = webdriver.Chrome('/usr/local/chromedriver')
+    url = "http://www.cnn.com"
+    driver.get(url)
+
+    section = driver.find_element_by_id("homepage1-zone-1")
+    headlines = section.find_elements_by_tag_name("article")
+
+    results = []
+    counter = 0
+
+    for headline in headlines:
+        url = headline.find_elements_by_tag_name("a")
+        title = headline.text
+
+        if len(title) > 0 and len(url) > 0:
+
+            obj = {
+                'site': 'CNN',
+                'title': title,
+                'url': url[0].get_attribute("href"),
+                'number': counter
+                }
+                
+            counter += 1
+        results.append(obj)
+
+    driver.close()
+    return results
+
+
+# FOX--------------------------------
+def get_fox():
+    driver = webdriver.Chrome('/usr/local/chromedriver')
+    url = "https://www.foxnews.com/"
+    driver.get(url)
+
+    # section = driver.find_element_by_class_name("collection-spotlight")
+    headlines = driver.find_elements_by_tag_name("article")
+
+    results = []
+    counter = 0
+
+    for headline in headlines:
+        url = headline.find_elements_by_tag_name("a")
+        title = headline.text
+
+        if len(title) > 0 and len(url) > 0:
+
+            obj = {
+                'site': 'FOX',
+                'title': title,
+                'url': url[0].get_attribute("href"),
+                'number': counter
+                }
+
+            counter += 1
+        results.append(obj)
+    
+    driver.close()
+    return results
+
+#  NY Times ----------------------------------
+def get_NYT():
+    driver = webdriver.Chrome('/usr/local/chromedriver')
+    url = "https://www.nytimes.com/"
+    driver.get(url)
+
+    headlines = driver.find_elements_by_tag_name("article")
+
+    results = []
+    counter = 0
+
+    for headline in headlines:
+        url = headline.find_elements_by_tag_name("a")
+        title = headline.text
+
+        if len(title) > 0 and len(url) > 0:
+
+            obj = {
+                'site': 'NYTimes',
+                'title': title,
+                'url': url[0].get_attribute("href"),
+                'number': counter
+            }
+
+            counter += 1
+        results.append(obj)
+
+    driver.close()
+    return results
+
+
+
+# Huffington Post
+
+def get_huff():
+    driver = webdriver.Chrome('/usr/local/chromedriver')
+    url = "https://www.huffingtonpost.com"
+    driver.get(url)
+
+    section_1 = driver.find_element_by_class_name(
+        "front-page-top").find_elements_by_class_name("card")
+    section_2 = driver.find_element_by_class_name(
+        "front-page-content").find_elements_by_class_name("card")
+    headlines = section_1 + section_2
+
+    results = []
+    counter = 0
+
+    for headline in headlines:
+        url = headline.find_elements_by_tag_name("a")
+        title = headline.text
+
+        if len(title) > 0 and len(url) > 0:
+            obj = {
+                    'site': 'Huffington Post',
+                    'title': title,
+                    'url': url[0].get_attribute("href"),
+                    'number': counter
+                }
+            counter += 1
+        results.append(obj)
+    driver.close()
+    return results
+
+
+# NBC ---------------------------
+def get_nbc():
+    driver = webdriver.Chrome('/usr/local/chromedriver')
+    url = "https://www.nbcnews.com/"
+    driver.get(url)
+
+    headlines = driver.find_elements_by_tag_name("article")
+
+    results = []
+    counter = 0
+
+    for headline in headlines:
+        url = headline.find_elements_by_tag_name("a")
+        title = headline.text
+
+        if len(title) > 0 and len(url) > 0:
+
+            obj = {
+                'site': 'NBC News',
+                'title': title,
+                'url': url[0].get_attribute("href"),
+                'number': counter
+            }
+
+            counter += 1
+        results.append(obj)
+
+    driver.close()
+    return results
+
+
+# Washington Post
+def get_post():
+    driver = webdriver.Chrome('/usr/local/chromedriver')
+    url = "https://www.washingtonpost.com/"
+    driver.get(url)
+
+    headlines = driver.find_elements_by_tag_name("article")
+
+    results = []
+    counter = 0
+
+    for headline in headlines:
+        url = headline.find_elements_by_tag_name("a")
+        title = headline.text
+
+        if len(title) > 0 and len(url) > 0:
+
+            obj = {
+                'site': 'NBC News',
+                'title': title,
+                'url': url[0].get_attribute("href"),
+                'number': counter
+            }
+
+            counter += 1
+        results.append(obj)
+
+    driver.close()
+    return results
