2c33a27c8d2578e164585798dee59c5f891b9a3a
Chris Parfitt <parfitt.chris7@gmail.com>
2020-01-20 16:38:42 -0500

    edit procfile

--- app.py
+++ app.py
@@ -109,6 +109,7 @@ def get_article(id):
 
 # Run Server
 if __name__ == '__main__':
-    app.run(threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     # app.run(debug=True);
 
